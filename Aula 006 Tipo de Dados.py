a80dc1f2807fc82af6d882babd63eab377b88c44
Rafael Nunes <rafaelnunes44@gmail.com>
2020-04-24 17:44:11 -0300

    Resolucao de alguns desafios

---
+++ Aula 006 Tipo de Dados.py
@@ -0,0 +1,11 @@
+
+"""
+bool   = (True/False)
+int    = (-4, 9 , 100 , -25)
+float  = (4.0, 3.2, -5.9)
+str    =  "Texto" or " "
+
+"""
+
+n1 = int(input('Digite um valor: '))
+print(type(n1))
--- Desafio 001.py
+++ Desafio 001.py
@@ -1,2 +1,50 @@
+
+#Desafio 01
+""""
+print ("Desafio 01")
 nome= input("Qual seu nome?")
-print("Olá!", nome, "Muito em te conhecer!")
+print("É um prazer te conhecer,{}!".format(nome))
+"""
+
+#Desafio 02
+"""
+print ("Desafio 02")
+dia=input("Qual dia você nasceu? ")
+mes=input("Qual mês? ")
+ano=input("Qual ano? ")
+print("Você nasceu no dia",dia,"mês de",mes,"ano de",ano,"Correto? ")
+"""
+
+#Desafio 03
+"""
+n1=float(input("Primeiro número: "))
+n2=float(input("Segundo número: "))
+soma = n1+n2
+print("A soma é:{} ".format(soma))
+"""
+
+#Desafio extra
+"""
+n1=float(input("Primeiro número: "))
+n2=float(input("Segundo número: "))
+soma = n1+n2
+print("A soma entre",n1,"e",n2,"é:",soma)
+"""
+
+#Desafio extra 01
+"""
+print('Faremos a soma entre dois números:')
+n1=float(input("Primeiro número: "))
+n2=float(input("Segundo número: "))
+soma = n1+n2
+print("A soma entre {} e {} vale {}".format(n1, n2, soma))
+"""
+
+#Desafio extra 02
+"""
+n= input('Digite um número: ')
+#print(n.isnumeric())
+#print(n.isalpha())
+#print(n.isalnum())
+
+"""
